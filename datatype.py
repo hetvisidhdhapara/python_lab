58e58b52735adb207cb8b8b0757e4708e388a21d
hetvisidhdhapara <hetvisidhdhapara@gmail.com>
2026-01-06 09:06:54 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,17 @@
+a = 10 #int
+print(a)
+a=10
+b=20
+print(a+b)
+x=hello world  #str
+print(x)
+x={"name":"john","age":"30"}  #dict,set
+print(x)
+x=bytearray(5) #bytearray
+print(x)
+x=["apple","banana","cherry"]  #list
+print(x)
+x=("apple","banana","cherry") #tuple
+print(x)
+x=frozenset({"apple","banana"}) #frozenset
+print(x)
