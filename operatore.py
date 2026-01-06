42a671d3cb815e0a380d4d88296f904240f18200
hetvisidhdhapara <hetvisidhdhapara@gmail.com>
2026-01-06 09:23:26 +0530

    Add files via upload

---
+++ operatore.py
@@ -0,0 +1,28 @@
+#operatore
+#arithmetic operatore
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+# assignment operatore
+x=15
+print(x)
+# unary minus
+a=10
+print(-a)
+# relational operatore
+p=20
+q=10
+print(p>q)
+print(p==q)
+# logical operators
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x) 
+# boolean
+is_pass=True
+print(is_pass)
