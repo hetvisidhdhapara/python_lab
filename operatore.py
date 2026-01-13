61516828c2ee2ffb88b94e4a025ea9d1ba551ecf
hetvisidhdhapara <hetvisidhdhapara@gmail.com>
2026-01-13 09:22:50 +0530

    Add files via upload

--- operatore.py
+++ operatore.py
@@ -26,3 +26,17 @@ print(not x)
 # boolean
 is_pass=True
 print(is_pass)
+#bitwise operators
+a=5
+b=3
+print(a&b)
+print(a/b)
+#membership operator 
+numbers=[1,2,3,4]
+print (2 in numbers)
+print(5 in numbers)
+#identity operators
+x=10
+y=10
+print(x is y)
+print(x is not y)
