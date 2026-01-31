4312e2d3fffdd0db6a48b011bb1bee41d38535ef
hetvisidhdhapara <hetvisidhdhapara@gmail.com>
2026-01-31 08:25:19 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,10 @@
+# OUTPUT using print()
+print("hello world")
+a=10
+print(a)
+
+# INPUT using input()
+name=input("enter your name:")
+print("hello",name)
+num=int(input("enter a number:"))
+print(num)
