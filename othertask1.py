4a624358544ec284bc92208d2c5ae842d6ee4959
hetvisidhdhapara <hetvisidhdhapara@gmail.com>
2026-03-05 23:09:50 +0530

    Add files via upload

---
+++ othertask1.py
@@ -0,0 +1,31 @@
+# 1.print 0 to 10 with while loop
+i=1
+while i <= 10:
+    print(i,end=" ")
+    i += 1
+
+# 2.print 0 to 10 with for loop
+
+for i in range(1,11):
+    print(i,end=" ")
+
+# 3. 1 even numbers up to 10
+
+i=0
+while i<=10:
+    print(i,end=" ")
+    i += 2
+
+# 4. 1 even numbers
+
+for i in range(30,51):
+    if i % 2 != 0:
+        print(i,end=" ")
+
+# 5. 2 odd numbers up to 10
+
+i=1
+while i<=10:
+    print(i,end=" ")
+    i += 2
+
