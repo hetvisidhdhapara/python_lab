b2d4ddc046bbafe5099f6347ea1c74f9fe947824
hetvisidhdhapara <hetvisidhdhapara@gmail.com>
2026-03-06 17:10:14 +0530

    Add files via upload

---
+++ othertask2.py
@@ -0,0 +1,33 @@
+# 6. even numbers from 10 to 30
+
+for i in range(10,31):
+    if i % 2 == 0:
+        print(i,end=" ")
+
+# 7. asecending 0 to 10
+
+i=0
+while i in range(0,10):
+    i += 1
+    print(i,end=" ")
+
+# 8. sum
+
+a=10
+b=10
+print(a+b)
+
+# 9.even numbers up to 10
+
+i=0
+while i <= 10:
+    print(i,end=" ")
+    i += 2
+
+# 10.decending 10 to 0
+
+i=10
+while i >= 0:
+    print(i,end=" ")
+    i-=1
+
