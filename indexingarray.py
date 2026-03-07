0f9ff13a13d76280af4903d697637980e2ecd27a
hetvisidhdhapara <hetvisidhdhapara@gmail.com>
2026-03-07 08:55:27 +0530

    Add files via upload

---
+++ indexingarray.py
@@ -0,0 +1,23 @@
+from array import array
+
+# positive indexing
+
+arr=array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])
+
+# negative indexing
+
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])
+
+# modifying elements using index
+
+arr=array('i',[10,20,30,40,50])
+arr[2]=35
+print(arr)
+
+
